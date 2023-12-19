a52a9018080874a91979447677fb2c62f3ca414e
scpflash <kolanvs@yandex.ru>
2023-12-19 23:32:59 +0300

    ANOTHER_COMMIT

--- yandex_weather_django/urls.py
+++ yandex_weather_django/urls.py
@@ -17,7 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from yandex_weather_django.views import YandexWeatherView
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-
+    path('weather/', YandexWeatherView.as_view())
 ]
--- yandex_weather_django/views.py
+++ yandex_weather_django/views.py
@@ -1,8 +1,25 @@
+import requests
+from cachetools.func import ttl_cache
 from django.http import JsonResponse
 from django.views import View
+from constants import cities
+from django import http
 
 
 class YandexWeatherView(View):
 
+    # @ttl_cache(maxsize=200, ttl=1800.0)
     def get(self, *args, **kwargs):
-        return JsonResponse({})
+
+        WEATHER_URL =
+
+        city = args[0].GET.get('city')
+        if not city:
+            return http.HttpResponseBadRequest(f'Не передано название города')
+        founded_dict = next(filter(lambda d: d.get('city') == city, cities), {})
+        if not founded_dict:
+            return http.HttpResponseBadRequest(f'Город {city} не найден')
+        url = f'https://api.weather.yandex.ru/v2/informers?lat={founded_dict["lat"]}&lon={founded_dict["lon"]}&lang=ru_RU'
+
+        result_json = requests.get(url, headers={})
+
